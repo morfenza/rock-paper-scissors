0de4464eeb5a4fa6481156e5cf33f91c6253b065
Morfenza <ianlourenco@protonmail.com>
2022-12-15 19:23:36 -0300

    Create computer choice function

--- game.js
+++ game.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+// Randomly return the computer choice 
+function getComputerChoice() {
+  let choicesArray = ["rock", "paper", "scissors"]
+
+  return choicesArray[Math.floor(Math.random() * choicesArray.length)]
+
+}
+
+console.log(getComputerChoice())
