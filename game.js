1dc6498025d0115559f6c8e150addc2da2b8c1c1
Morfenza <ianlourenco@protonmail.com>
2022-12-15 19:45:30 -0300

    Create playRound function

--- game.js
+++ game.js
@@ -3,7 +3,44 @@ function getComputerChoice() {
   let choicesArray = ["rock", "paper", "scissors"]
 
   return choicesArray[Math.floor(Math.random() * choicesArray.length)]
-
 }
 
-console.log(getComputerChoice())
+function playRound(computerChoice, playerChoice) {
+  
+  if (playerChoice === "rock") {
+    if (computerChoice === "rock") {
+      return "Draw! Both players chose Rock"
+
+    } else if (computerChoice === "paper") {
+      return "You Lose! Paper beats Rock"
+    
+    } else {
+      return "You Win! Rock beats Scissors"
+    }
+  } else if (playerChoice === "paper") {
+    if (computerChoice === "paper") {
+      return "Draw! Both players chose Paper"
+
+    } else if (computerChoice === "rock") {
+      return "You Win! Paper beats Rock"
+    
+    } else {
+      return "You Lose! Scissors beats Paper"
+    }
+  } else {
+    if (computerChoice === "scissors") {
+      return "Draw! Both players chose Scissors"
+
+    } else if (computerChoice === "rock") {
+      return "You Lose! Rock beats Scissors"
+    
+    } else {
+      return "You Win! Scissors beats Paper"
+    }
+  }
+}
+
+let computerChoice = getComputerChoice()
+let playerChoice = "rock"
+
+console.log(playRound(computerChoice, playerChoice))
