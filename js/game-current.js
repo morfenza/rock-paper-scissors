4355d8126a27be5cf2cc68763373e285db2af538
Morfenza <ianlourenco@protonmail.com>
2023-01-02 18:48:15 -0300

    Create victory check && result counter

--- js/game-current.js
+++ js/game-current.js
@@ -1,8 +1,8 @@
 // Randomly return the computer choice 
 function getComputerChoice() {
-  let choicesArray = ["rock", "paper", "scissors"]
+  let choicesArray = ["rock", "paper", "scissors"];
 
-  return choicesArray[Math.floor(Math.random() * choicesArray.length)]
+  return choicesArray[Math.floor(Math.random() * choicesArray.length)];
 }
 
 // Play a single round
@@ -10,51 +10,91 @@ function playRound(computerChoice, playerChoice) {
   
   if (playerChoice === "rock") {
     if (computerChoice === "rock") {
-      result.textContent = 'Draw! Both players chose Rock'
+      resultDiv.textContent = 'Draw! Both players chose Rock';
 
     } else if (computerChoice === "paper") {
-      result.textContent = 'You Lose! Paper beats Rock'
+      resultDiv.textContent = 'You Lose! Paper beats Rock';
+      computerCounter++;
+      cCounterSpan.textContent = computerCounter;
 
     } else {
-      result.textContent = 'You Win! Rock beats Scissors'
-    }
+      resultDiv.textContent = 'You Win! Rock beats Scissors';
+      humanCounter++;
+      hCounterSpan.textContent = humanCounter;
 
+    }
   } else if (playerChoice === "paper") {
     if (computerChoice === "paper") {
-      result.textContent = 'Draw! Both players chose Paper'
+      resultDiv.textContent = 'Draw! Both players chose Paper';
 
     } else if (computerChoice === "rock") {
-      result.textContent = 'You Win! Paper beats Rock'
+      resultDiv.textContent = 'You Win! Paper beats Rock';
+      humanCounter++;
+      hCounterSpan.textContent = humanCounter;
 
     } else {
-      result.textContent = 'You Lose! Scissors beats Paper'
-    }
+      resultDiv.textContent = 'You Lose! Scissors beats Paper';
+      computerCounter++;
+      cCounterSpan.textContent = computerCounter;
 
+    }
   } else {
     if (computerChoice === "scissors") {
-      result.textContent = 'Draw! Both players chose Scissors'
+      resultDiv.textContent = 'Draw! Both players chose Scissors';
 
     } else if (computerChoice === "rock") {
-      result.textContent = 'You Lose! Rock beats Scissors'
-    
+      resultDiv.textContent = 'You Lose! Rock beats Scissors';
+      computerCounter++;
+      cCounterSpan.textContent = computerCounter;
+
     } else {
-      result.textContent = 'You Lose! Rock beats Scissors'
+      resultDiv.textContent = 'You Win! Scissors beats Paper';
+      humanCounter++;
+      hCounterSpan.textContent = humanCounter;
+
     }
   }
 }
-const result = document.querySelector('#result')
 
-const rockBtn = document.querySelector('#rock')
+function checkVictory() {
+  if (humanCounter == 5) {
+    resultDiv.textContent = "Congratulations! You've won the game!!!"
+    computerCounter = 0;
+    humanCounter = 0;
+    cCounterSpan.textContent = computerCounter;
+    hCounterSpan.textContent = humanCounter;
+
+  } else if (computerCounter == 5) {
+    resultDiv.textContent = "Oh no! You've lost the game!!"
+    computerCounter = 0;
+    humanCounter = 0;
+    cCounterSpan.textContent = computerCounter;
+    hCounterSpan.textContent = humanCounter;
+  }
+}
+
+const resultDiv = document.querySelector('#result');
+const hCounterSpan = document.querySelector('#h-counter');
+const cCounterSpan = document.querySelector('#c-counter');
+
+let computerCounter = 0;
+let humanCounter = 0;
+
+
+const rockBtn = document.querySelector('#rock');
 rockBtn.addEventListener('click', () => {
   playRound(getComputerChoice(), 'rock')
+  checkVictory()
 }); 
 
-const paperBtn = document.querySelector('#paper')
+const paperBtn = document.querySelector('#paper');
 paperBtn.addEventListener('click', () => {
   playRound(getComputerChoice(), 'paper')
+  checkVictory()
 }); 
 
-const scissorsBtn = document.querySelector('#scissors')
+const scissorsBtn = document.querySelector('#scissors');
 scissorsBtn.addEventListener('click', () => {
   playRound(getComputerChoice(), 'scissors')
+  checkVictory()
 }); 
